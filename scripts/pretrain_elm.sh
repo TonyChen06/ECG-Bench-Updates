28ca72f43056d2520b610aabec76af5d8d539a6c
Tony <tc3416@columbia.edu>
2025-12-26 12:01:28 +0000

    script

--- scripts/pretrain_elm.sh
+++ scripts/pretrain_elm.sh
@@ -7,9 +7,9 @@ python -m ecg_bench.pretrain_elm \
     --peft \
     --attention_type flash_attention_2 \
     --device cuda:0 \
-    --epochs 3 \
+    --epochs 1 \
     --batch_size 4 \
     --lr 1e-4 \
     --llm_input_len 2048 \
-    --warmup 500 \
+    --warmup 5000 \
     --dev
